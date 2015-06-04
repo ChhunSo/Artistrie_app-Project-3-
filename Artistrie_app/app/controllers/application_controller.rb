a72d026dc8341806e1b7cd5fe613b9eea6be373e
Jeffrey Ercolani <Jercolani@JeffyMBP.local>
2015-06-03 17:48:00 -0700

    create all models, views, associations

--- Artistrie_app/app/controllers/application_controller.rb
+++ Artistrie_app/app/controllers/application_controller.rb
@@ -1,5 +1,22 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  include Pundit
+
+  protected
+  def after_sign_in_path_for(resource_or_scope)
+    @user = current_user
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up).push(:user_type, :username)
+  end
+  
 end
